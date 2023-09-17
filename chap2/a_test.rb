1ec5e374c561fce44785534f2b0e1fdb6c3dda16
mohira <shukatsudaison@gmail.com>
2023-09-17 17:45:49 +0900

    chap2: 古いシグネチャのやつを修正するのがだるので、封印

--- chap2/a_test.rb
+++ chap2/a_test.rb
@@ -11,44 +11,47 @@ class ATest < Minitest::Test
     assert_equal Number.new(7), m.run
   end
 
-  def test_add_multipy_expression
-    # (1x2)+(3x4)
-    expression = Add.new(
-      Multiply.new(Number.new(1), Number.new(2)),
-      Multiply.new(Number.new(3), Number.new(4)),
-    )
+  class Archived
+    def test_add_multipy_expression
+      # (1x2)+(3x4)
+      expression = Add.new(
+        Multiply.new(Number.new(1), Number.new(2)),
+        Multiply.new(Number.new(3), Number.new(4)),
+      )
 
-    assert expression.reducible?
-    r1 = Add.new(Number.new(2), Multiply.new(Number.new(3), Number.new(4)))
-    assert_equal r1, expression.reduce
+      assert expression.reducible?
+      r1 = Add.new(Number.new(2), Multiply.new(Number.new(3), Number.new(4)))
+      assert_equal r1, expression.reduce
 
-    r2 = Add.new(Number.new(2), Number.new(12))
-    assert_equal r2, expression.reduce.reduce
+      r2 = Add.new(Number.new(2), Number.new(12))
+      assert_equal r2, expression.reduce.reduce
 
-    r3 = Number.new(14)
-    assert_equal r3, expression.reduce.reduce.reduce
+      r3 = Number.new(14)
+      assert_equal r3, expression.reduce.reduce.reduce
 
-    refute expression.reduce.reduce.reduce.reducible?
+      refute expression.reduce.reduce.reduce.reducible?
+    end
+
+    def test_machine
+      expression = Add.new(
+        Multiply.new(Number.new(1), Number.new(2)),
+        Multiply.new(Number.new(3), Number.new(4)),
+      )
+
+      machine = Machine.new(expression)
+
+      assert_equal Number.new(14), machine.run
+    end
+
+    def test_less_than_to_boolean
+      # 1+2 < 3*4 -> true
+      expression = LessThan.new(
+        Add.new(Number.new(1), Number.new(2)),
+        Multiply.new(Number.new(3), Number.new(4)),
+      )
+
+      assert_equal Boolean.new(true), Machine.new(expression).run
+    end
   end
 
-  def test_machine
-    expression = Add.new(
-      Multiply.new(Number.new(1), Number.new(2)),
-      Multiply.new(Number.new(3), Number.new(4)),
-    )
-
-    machine = Machine.new(expression)
-
-    assert_equal Number.new(14), machine.run
-  end
-
-  def test_less_than_to_boolean
-    # 1+2 < 3*4 -> true
-    expression = LessThan.new(
-      Add.new(Number.new(1), Number.new(2)),
-      Multiply.new(Number.new(3), Number.new(4)),
-    )
-
-    assert_equal Boolean.new(true), Machine.new(expression).run
-  end
 end
