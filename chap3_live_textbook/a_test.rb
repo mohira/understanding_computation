c1a777f3cd5069abfdb18a48b7abc8bb24bad010
mohira <shukatsudaison@gmail.com>
2023-09-30 20:58:55 +0900

    chap3: 文字を読めるようになったよ！

--- chap3_live_textbook/a_test.rb
+++ chap3_live_textbook/a_test.rb
@@ -29,4 +29,29 @@ class Chap3Test < Minitest::Test
     refute DFA.new(2, [1, 3], rulebook).accepting?
     refute DFA.new(1, [3], rulebook).accepting?
   end
+
+  def test_p68_read_character
+    rules = [
+      FARule.new(1, 'a', 2), FARule.new(1, 'b', 1),
+      FARule.new(2, 'a', 2), FARule.new(2, 'b', 3),
+      FARule.new(3, 'a', 3), FARule.new(3, 'b', 3),
+    ]
+    rulebook = DFARulebook.new(rules)
+
+    dfa = DFA.new(1, [3], rulebook)
+    assert_equal 1, dfa.current_state
+    refute dfa.accepting?
+
+    dfa.read_character('b')
+    assert_equal 1, dfa.current_state
+    refute dfa.accepting?
+
+    3.times { dfa.read_character('a') }
+    assert_equal 2, dfa.current_state
+    refute dfa.accepting?
+
+    dfa.read_character('b')
+    assert_equal 3, dfa.current_state
+    assert dfa.accepting?
+  end
 end
