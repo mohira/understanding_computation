4416fe2348a76d4dacf414198ae4a67b10d0c1be
mohira <shukatsudaison@gmail.com>
2023-10-01 22:48:31 +0900

    chap3: NFAをシミュレータ！

--- chap3_live_textbook/nfa.rb
+++ chap3_live_textbook/nfa.rb
@@ -1,4 +1,32 @@
 # frozen_string_literal: true
 
-class NFA
+require 'set'
+
+class FARule < Struct.new(:state, :character, :next_state)
+  def applies_to?(state, character)
+    (self.state == state) && (self.character == character)
+  end
+
+  def follow
+    # 規則を適用するときに機械をどのように変更するかを返す #follow メソッドを持ちます
+    next_state
+  end
+
+  def inspect
+    "#<FARule  #{state.inspect} --#{character}--> #{next_state.inspect}>"
+  end
+end
+
+class NFARuleBook < Struct.new(:rules)
+  def next_states(states, character)
+    states.flat_map { |state| follow_rules_for(state, character) }.to_set
+  end
+
+  def follow_rules_for(state, character)
+    rules_for(state, character).map(&:follow)
+  end
+
+  def rules_for(state, character)
+    rules.select { |rule| rule.applies_to?(state, character) }
+  end
 end
--- chap3_live_textbook/nfa_test.rb
+++ chap3_live_textbook/nfa_test.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
+require 'set'
+require 'minitest/autorun'
+require_relative 'nfa'
 
 class NFATest < Minitest::Test
-  def test_p1; end
+  def test_p74_nfa_1
+    # 図3-5
+    rules = [
+      FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
+      FARule.new(2, 'a', 3), FARule.new(2, 'b', 3),
+      FARule.new(3, 'a', 4), FARule.new(3, 'b', 4)
+    ]
+
+    rulebook = NFARuleBook.new(rules)
+
+    # p rulebook
+    assert_equal Set[1, 2], rulebook.next_states(Set[1], 'b')
+    assert_equal Set[1, 3], rulebook.next_states(Set[1, 2], 'a')
+    assert_equal Set[1, 2, 4], rulebook.next_states(Set[1, 3], 'b')
+  end
+
 end
